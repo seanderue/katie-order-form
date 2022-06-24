3a0eeb40a22ff1ca5261b6392cb2d6e2abc59430
Sean <Sean@MacBook-Pro-2.local>
2022-06-24 14:15:28 -0400

    updated site name

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Logo({menuOpen}) {
   return (
     <div className='Logo'>
-        <h1 className={menuOpen ? 'Logo-text open' : 'Logo-text'} data-text="Katie Ann Bakes">Katie Ann Bakes</h1>
+        <h1 className={menuOpen ? 'Logo-text open' : 'Logo-text'} data-text="Katie Ann's Kitchen">Katie Ann's Kitchen</h1>
     </div>
   )
 }
